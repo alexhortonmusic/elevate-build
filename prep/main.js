f632c2439fedb7182692cb1d1cc63ee560b8f51a
Alex Horton <alexhortonmusic@gmail.com>
2017-01-20 10:28:15 -0600

    nav changes dynamically

--- prep/main.js
+++ prep/main.js
@@ -1,10 +1,12 @@
+
+// darkens nav bar on scroll
 $(window).on('scroll', function() {
   let navHeight = $('nav').height()
-  let stop = $(window).scrollTop()
+  let change = $(window).scrollTop()
   let nav = $('#header nav')
-  if (stop > navHeight) {
-    nav.css('background-color', 'rgba(68, 69, 69, 0.9)')
-  } else if (stop < navHeight) {
-    nav.css('background-color', 'rgba(68, 69, 69, 0.5)')
+  if (change > navHeight) {
+    nav.addClass('navSolid')
+  } else if (change < navHeight) {
+    nav.removeClass('navSolid')
   }
 })
